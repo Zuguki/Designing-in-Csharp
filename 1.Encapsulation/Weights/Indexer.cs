eeb4010e24d040a1cf78a0247f96b40f7779f0c0
Zuguki <guskov.zahar20@gmail.com>
2022-06-23 17:50:28 +0300

    Done for Ulearn

--- 1.Encapsulation/Weights/Indexer.cs
+++ 1.Encapsulation/Weights/Indexer.cs
@@ -1,10 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Incapsulation.Weights
 {
-	
+    public class Indexer
+    {
+        private double[] Doubles { get; }
+        private int Start { get; }
+        public int Length { get; }
+
+        public Indexer(double[] doubles, int start, int length)
+        {
+            CheckArguments(doubles, start, length);
+            Doubles = doubles;
+            Start = start;
+            Length = length;
+        }
+
+        public double this[int index]
+        {
+            get
+            {
+                CheckIndex(index);
+                return Doubles[Start + index];
+            }
+            set
+            {
+                CheckIndex(index);
+                Doubles[Start + index] = value;
+            }
+        }
+
+        private void CheckIndex(int index)
+        {
+            if (Start + index < 0 || Start + index > Length || index < 0)
+                throw new IndexOutOfRangeException();
+        }
+
+        private static void CheckArguments(IReadOnlyCollection<double> doubles, int start, int length)
+        {
+            if (start < 0 || start > doubles.Count || length < 0 || length + start > doubles.Count)
+                throw new ArgumentException();
+        }
+    }
 }
