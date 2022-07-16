f35bb76bb842dec0f64220f6b930598bc833dae4
Zuguki <guskov.zahar20@gmail.com>
2022-07-16 14:31:03 +0300

    BoundingBoxVisitor done

--- 2.Inheritance/Geometry/Visitor/VisitorTask.cs
+++ 2.Inheritance/Geometry/Visitor/VisitorTask.cs
@@ -1,8 +1,10 @@
 
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Inheritance.Geometry.Visitor
 {
+
     public abstract class Body
     {
         public Vector3 Position { get; }
@@ -11,6 +13,12 @@ namespace Inheritance.Geometry.Visitor
         {
             Position = position;
         }
+
+        public abstract object Accept(IVisitor visitor);
+
+        public abstract bool ContainsPoint(Vector3 point);
+
+        public abstract RectangularCuboid GetBoundingBox();
     }
 
     public class Ball : Body
@@ -21,6 +29,18 @@ namespace Inheritance.Geometry.Visitor
         {
             Radius = radius;
         }
+
+        public override object Accept(IVisitor visitor) => visitor.Visit(this);
+
+        public override bool ContainsPoint(Vector3 point)
+        {
+            var vector = point - Position;
+            var length2 = vector.GetLength2();
+            return length2 <= Radius * Radius;
+        }
+
+        public override RectangularCuboid GetBoundingBox() => 
+            new RectangularCuboid(Position, Radius * 2, Radius * 2, Radius * 2);
     }
 
     public class RectangularCuboid : Body
@@ -35,6 +55,23 @@ namespace Inheritance.Geometry.Visitor
             SizeY = sizeY;
             SizeZ = sizeZ;
         }
+
+        public Vector3 MinPoint => new Vector3(
+            Position.X - SizeX / 2,
+            Position.Y - SizeY / 2,
+            Position.Z - SizeZ / 2);
+
+        public Vector3 MaxPoint => new Vector3(
+            Position.X + SizeX / 2,
+            Position.Y + SizeY / 2,
+            Position.Z + SizeZ / 2);
+
+        public override object Accept(IVisitor visitor) => visitor.Visit(this);
+
+        public override bool ContainsPoint(Vector3 point) =>
+            point >= MinPoint && point <= MaxPoint;
+
+        public override RectangularCuboid GetBoundingBox() => this;
     }
 
     public class Cylinder : Body
@@ -48,6 +85,22 @@ namespace Inheritance.Geometry.Visitor
             SizeZ = sizeZ;
             Radius = radius;
         }
+
+        public override object Accept(IVisitor visitor) => visitor.Visit(this);
+
+        public override bool ContainsPoint(Vector3 point)
+        {
+            var vectorX = point.X - Position.X;
+            var vectorY = point.Y - Position.Y;
+            var length2 = vectorX * vectorX + vectorY * vectorY;
+            var minZ = Position.Z - SizeZ / 2;
+            var maxZ = minZ + SizeZ;
+
+            return length2 <= Radius * Radius && point.Z >= minZ && point.Z <= maxZ;
+        }
+
+        public override RectangularCuboid GetBoundingBox() =>
+            new RectangularCuboid(Position, Radius * 2, Radius * 2, SizeZ);
     }
 
     public class CompoundBody : Body
@@ -58,15 +111,70 @@ namespace Inheritance.Geometry.Visitor
         {
             Parts = parts;
         }
+
+        public override object Accept(IVisitor visitor) => visitor.Visit(this);
+
+        public override bool ContainsPoint(Vector3 point) => Parts.Any(body => body.ContainsPoint(point));
+        
+        public override RectangularCuboid GetBoundingBox()
+        {
+            (double min, double max) x = (0, 0);
+            (double min, double max) y = (0, 0);
+            (double min, double max) z = (0, 0);
+
+            var boxes = Parts.Select(box => box.GetBoundingBox()).ToList();
+            x.min = boxes.Min(box => box.MinPoint.X);
+            y.min = boxes.Min(box => box.MinPoint.Y);
+            z.min = boxes.Min(box => box.MinPoint.Z);
+
+            x.max = boxes.Max(box => box.MaxPoint.X);
+            y.max = boxes.Max(box => box.MaxPoint.Y);
+            z.max = boxes.Max(box => box.MaxPoint.Z);
+            
+            return new RectangularCuboid(new Vector3((x.max + x.min) / 2, (y.max + y.min) / 2, (z.max + z.min) / 2),
+                x.max - x.min, y.max - y.min, z.max - z.min);
+        }
     }
 
-    public class BoundingBoxVisitor
+    public class BoundingBoxVisitor : IVisitor
     {
-        //TODO
+        public RectangularCuboid Visit(Ball ball) => ball.GetBoundingBox();
+
+        public RectangularCuboid Visit(Cylinder cylinder) => cylinder.GetBoundingBox();
+
+        public RectangularCuboid Visit(RectangularCuboid rectangularCuboid) => rectangularCuboid.GetBoundingBox();
+
+        public RectangularCuboid Visit(CompoundBody compoundBody) => compoundBody.GetBoundingBox();
     }
 
-    public class BoxifyVisitor
+    public class BoxifyVisitor : IVisitor
     {
-        //TODO
+        public RectangularCuboid Visit(Ball ball)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RectangularCuboid Visit(Cylinder cylinder)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RectangularCuboid Visit(RectangularCuboid rectangularCuboid)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public RectangularCuboid Visit(CompoundBody compoundBody)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public interface IVisitor
+    {
+        RectangularCuboid Visit(Ball ball);
+        RectangularCuboid Visit(Cylinder cylinder);
+        RectangularCuboid Visit(RectangularCuboid rectangularCuboid);
+        RectangularCuboid Visit(CompoundBody compoundBody);
     }
 }
