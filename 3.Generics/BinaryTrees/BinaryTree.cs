680e659aa94f90488355a747b15cd3aa23ff0279
Zuguki <guskov.zahar20@gmail.com>
2022-07-27 20:58:09 +0300

    First test done

--- 3.Generics/BinaryTrees/BinaryTree.cs
+++ 3.Generics/BinaryTrees/BinaryTree.cs
@@ -5,25 +5,72 @@ using System.Collections.Generic;
 namespace Generics.BinaryTrees
 {
     public class BinaryTree<T> : IEnumerable<T>
+        where T : IComparable
     {
-        private List<T> _items = new List<T>();
-        
+        public BinaryTree<T> Parent;
+        public BinaryTree<T> Left;
+        public BinaryTree<T> Right;
+
+        public T Value;
+
         public BinaryTree()
         {
-            
+            Parent = this;
+        }
+
+        public BinaryTree(BinaryTree<T> parent)
+        {
+            Parent = parent;
+        }
+
+        public BinaryTree(BinaryTree<T> parent, T item) : this(parent)
+        {
+            Value = item;
         }
 
         public void Add(T item)
         {
-            _items.Add(item);
+            Add(this, item);
         }
 
-        public T Value()
+        private void Add(BinaryTree<T> current, T item)
         {
+            if (current.Value == null)
+            {
+                current = new BinaryTree<T>(current);
+                return;
+            }
             
+            switch (item.CompareTo(current.Value) < 0)
+            {
+                case true when current.Left.Value == null:
+                    current.Left = new BinaryTree<T>(current, item);
+                    break;
+                case true when current.Left.Value != null:
+                    Add(current.Left, item);
+                    break;
+            }
+            
+            
+            switch (item.CompareTo(current.Value) > 0)
+            {
+                case true when current.Right.Value == null:
+                    current.Right = new BinaryTree<T>(current, item);
+                    break;
+                case true when current.Right.Value != null:
+                    Add(current.Right, item);
+                    break;
+            }
         }
 
-        public IEnumerator<T> GetEnumerator() => ((IEnumerable<T>) _items).GetEnumerator();
+        public IEnumerator<T> GetEnumerator()
+        {
+            var left = Left;
+            while (left != null)
+                left = left.Left;
+            
+            yield break;
+        }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
