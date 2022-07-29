b6974d042dfb6668bcfe1a0811d454a9d9343702
Zuguki <guskov.zahar20@gmail.com>
2022-07-29 20:47:27 +0300

    Ulearn tests done

--- 3.Generics/BinaryTrees/BinaryTree.cs
+++ 3.Generics/BinaryTrees/BinaryTree.cs
@@ -10,50 +10,21 @@ namespace Generics.BinaryTrees
         public BinaryTree<T> Left;
         public BinaryTree<T> Right;
 
-        private bool _isDefault = true;
-
         public T Value;
 
-        public BinaryTree()
-        { }
+        private bool _isDefault = true;
 
-        public BinaryTree(T item)
+        public BinaryTree()
+        {
+        }
+
+        private BinaryTree(T item)
         {
             Value = item;
             _isDefault = false;
         }
 
         public void Add(T item) => Add(this, item);
-
-        private void Add(BinaryTree<T> current, T item)
-        {
-            if (current._isDefault)
-            {
-                current._isDefault = false;
-                current.Value = item;
-                return;
-            }
-            
-            switch (item.CompareTo(current.Value) <= 0)
-            {
-                case true when current.Left == null:
-                    current.Left = new BinaryTree<T>(item);
-                    break;
-                case true when current.Left != null:
-                    Add(current.Left, item);
-                    break;
-            }
-            
-            switch (item.CompareTo(current.Value) > 0)
-            {
-                case true when current.Right == null:
-                    current.Right = new BinaryTree<T>(item);
-                    break;
-                case true when current.Right != null:
-                    Add(current.Right, item);
-                    break;
-            }
-        }
 
         public IEnumerator<T> GetEnumerator()
         {
@@ -63,13 +34,33 @@ namespace Generics.BinaryTrees
 
             if (!_isDefault)
                 yield return Value;
-            
+
             if (Right != null)
                 foreach (var node in Right)
                     yield return node;
         }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        private static void Add(BinaryTree<T> current, T item)
+        {
+            if (current._isDefault)
+            {
+                current._isDefault = false;
+                current.Value = item;
+                return;
+            }
+
+            if (item.CompareTo(current.Value) <= 0 && current.Left == null)
+                current.Left = new BinaryTree<T>(item);
+            else if (item.CompareTo(current.Value) <= 0 && current.Left != null)
+                Add(current.Left, item);
+
+            if (item.CompareTo(current.Value) > 0 && current.Right == null)
+                current.Right = new BinaryTree<T>(item);
+            else if (item.CompareTo(current.Value) > 0 && current.Right != null)
+                Add(current.Right, item);
+        }
     }
 
     public class BinaryTree
