7a59203e2dd7a19c74ee32a9556dc94f46951419
Zuguki <guskov.zahar20@gmail.com>
2022-08-09 12:45:21 +0300

    ITransformer was added

--- _Photoshop/Filters/Transform/ITransformer.cs
+++ _Photoshop/Filters/Transform/ITransformer.cs
@@ -1,7 +1,14 @@
+using System.Drawing;
+
 namespace MyPhotoshop.Filters
 {
-    public interface ITransformer
+    public interface ITransformer<TParameters>
+        where TParameters : IParameters, new()
     {
+        void Prepare(Size size, TParameters parameters);
         
+        Size Result { get; }
+        
+        Point PointMap(Point newPoint);
     }
 }
