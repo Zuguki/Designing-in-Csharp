c4493d8df4582448329c2bcf73017ad82cd66d38
Zuguki <guskov.zahar20@gmail.com>
2022-08-29 13:58:21 +0300

    Done withou delegations

--- 4.Delegation/TreeTraversal/Traversal.cs
+++ 4.Delegation/TreeTraversal/Traversal.cs
@@ -10,12 +10,26 @@ namespace Delegates.TreeTraversal
     {
         public static IEnumerable<Product> GetProducts(ProductCategory root)
         {
-            throw new NotImplementedException();
+            if (root == null)
+                yield break;
+
+            foreach (var product in root.Categories.SelectMany(GetProducts))
+                yield return product;
+
+            foreach (var product in root.Products)
+                yield return product;
         }
 
         public static IEnumerable<Job> GetEndJobs(Job root)
         {
-            throw new NotImplementedException();
+            if (root == null)
+                yield break;
+
+            if (root.Subjobs.Count == 0)
+                yield return root;
+            else
+                foreach (var endJob in root.Subjobs.SelectMany(GetEndJobs))
+                    yield return endJob;
         }
 
         public static IEnumerable<T> GetBinaryTreeValues<T>(BinaryTree<T> root)
