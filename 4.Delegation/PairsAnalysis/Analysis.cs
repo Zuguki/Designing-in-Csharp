3e76faaa563c23eaad52fd300b36f6befed62673
Zuguki <guskov.zahar20@gmail.com>
2022-08-29 16:11:27 +0300

    FindMaxIndex done

--- 4.Delegation/PairsAnalysis/Analysis.cs
+++ 4.Delegation/PairsAnalysis/Analysis.cs
@@ -10,12 +10,60 @@ namespace Delegates.PairsAnalysis
     {
         public static int FindMaxPeriodIndex(params DateTime[] data)
         {
+            var pairs = data.Pairs();
+            return pairs.MaxIndex();
             return new MaxPauseFinder().Analyze(data);
         }
 
         public static double FindAverageRelativeDifference(params double[] data)
         {
             return new AverageDifferenceFinder().Analyze(data);
+        }
+    }
+
+    public static class Extention
+    {
+        public static int MaxIndex(this IEnumerable<Tuple<DateTime, DateTime>> items)
+        {
+            var itemsArray = items as Tuple<DateTime, DateTime>[] ?? items.ToArray();
+            if (itemsArray.Length == 0)
+                throw new ArgumentException();
+
+            var maxIndex = 0;
+            var maxValue = 0d;
+            for (var index = 0; index < itemsArray.Length; index++)
+            {
+                var value = Math.Abs((itemsArray[index].Item1 - itemsArray[index].Item2).TotalSeconds);
+                if (value <= maxValue) 
+                    continue;
+                
+                maxValue = value;
+                maxIndex = index;
+            }
+
+            return maxIndex;
+        }
+        
+        public static IEnumerable<Tuple<T, T>> Pairs<T>(this IEnumerable<T> items)
+        {
+            var itemsArray = items as T[] ?? items.ToArray();
+            if (itemsArray.Length < 2)
+                throw new InvalidOperationException();
+
+            var previewItem = default(T);
+            var counter = 0;
+
+            foreach (var item in itemsArray)
+            {
+                if (counter++ == 0)
+                {
+                    previewItem = item;
+                    continue;
+                }
+
+                yield return new Tuple<T, T>(previewItem, item);
+                previewItem = item;
+            }
         }
     }
 }
