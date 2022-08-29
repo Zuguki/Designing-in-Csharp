8504871f3b416b0dda096fa6613c0ec1f233e450
Zuguki <guskov.zahar20@gmail.com>
2022-08-29 11:18:03 +0300

    Done with EventHandler

--- 4.Delegation/Observers/ObservableStack.cs
+++ 4.Delegation/Observers/ObservableStack.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework.Internal;
 
 namespace Delegates.Observers
 {	
@@ -14,7 +10,7 @@ namespace Delegates.Observers
 
 		public void SubscribeOn<T>(ObservableStack<T> stack)
 		{
-			stack.HandleEvent += (eventData) => _log.Append(eventData);
+			stack.HandleEvent += (sender, eventData) => _log.Append(eventData);
 		}
 
 		public string GetLog() => _log.ToString();
@@ -22,15 +18,14 @@ namespace Delegates.Observers
 
 	public class ObservableStack<T>
 	{
-		public delegate void HandleDelegate(object eventData);
-		public event HandleDelegate HandleEvent;
+		public EventHandler<object> HandleEvent;
 		
 		private readonly List<T> _data = new List<T>();
 
 		public void Push(T obj)
 		{
 			_data.Add(obj);
-			HandleEvent?.Invoke(new StackEventData<T> {IsPushed = true, Value = obj});
+			HandleEvent?.Invoke(this, new StackEventData<T> {IsPushed = true, Value = obj});
 		}
 
 		public T Pop()
@@ -38,7 +33,7 @@ namespace Delegates.Observers
 			if (_data.Count == 0)
 				throw new InvalidOperationException();
 			var result = _data[_data.Count - 1];
-			HandleEvent?.Invoke(new StackEventData<T> {IsPushed = false, Value = result});
+			HandleEvent?.Invoke(this, new StackEventData<T> {IsPushed = false, Value = result});
 			return result;
 		}
 	}
