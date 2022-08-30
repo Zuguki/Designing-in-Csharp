7f2278c14b654ccf124bd55485ddf1ad688b1741
Zuguki <guskov.zahar20@gmail.com>
2022-08-30 06:41:58 +0300

    2. Practice done

--- _Photoshop/Filters/LighteningParameters.cs
+++ _Photoshop/Filters/LighteningParameters.cs
@@ -3,6 +3,6 @@ namespace MyPhotoshop.Filters
     public class LighteningParameters : IParameters
     {
         [ParameterInfo(Name = "Коэффициент", MaxValue = 10, MinValue = 0, Increment = 0.1, DefaultValue = 1)]
-        public double Ratio { get; private set; }
+        public double Ratio { get; set; }
     }
 }
---
+++ _Photoshop/Profiler/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using MyPhotoshop.Filters;
+using MyPhotoshop.Filters.Parameters;
+
+namespace Profiler
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Test((values, pairs) => pairs.SetValues(values), 100_000);
+            Test((values, pairs) => pairs.Ratio = values[0], 100_000);
+        }
+
+        public static void Test(Action<double[], LighteningParameters> action, int upperBound)
+        {
+            var args = new double[] {0};
+            var obj = new LighteningParameters();
+            action(args, obj);
+
+            var watch = new Stopwatch();
+            watch.Start();
+            for (var counter = 0; counter < upperBound; counter++)
+                action(args, obj);
+            watch.Stop();
+
+            Console.WriteLine(1000 * (double) watch.ElapsedMilliseconds / upperBound + " сек");
+        }
+    }
+}
